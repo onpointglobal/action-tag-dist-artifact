ccfec6998f65f6f595037d305ebf51e5c976dcd4
Renzo Mayer <rmayer@onpointglobal.com>
2021-03-18 19:15:44 -0300

    try move to tmp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -l
 
 tag_version=$1
+distignore=.distignore
 wp_version=$(echo $1 | cut -c 2-) 
 git config --global user.name github-actions
 git config --global user.email github-actions@github.com
+mv .distignore /tmp/.gitignore
 yarn install
 yarn build
 git checkout -b release-$tag_version
+mv /tmp/.gitignore .
 sed -i 's/\(Version: \).*/Version: '"$wp_version"'/g' style.css
-mv .distignore .gitignore
 git rm -r --cached .
 git add .
 git commit -am 'Created Tag '"$tag_version"''
