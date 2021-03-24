d1596a7025268a11c0c8b0fcef364f7c4beeb4e5
Renzo Mayer <rmayer@onpointglobal.com>
2021-03-24 13:16:32 -0300

    change var declaration

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 tag_version=$1
 distignore=.distignore
-commit_description= $(git log -1 --pretty=%B)
 wp_version=$(echo $1 | cut -c 2-) 
 git config --global user.name github-actions
 git config --global user.email github-actions@github.com
@@ -15,12 +14,12 @@ then
     exit 1
 fi
 git checkout -b release-$tag_version
-
 mv /tmp/.gitignore .
 sed -i 's/\(Version: \).*/Version: '"$wp_version"'/g' style.css
 git rm -r --cached .
 git add .
-git commit -am 'Created Tag '"$tag_version"''
+commit_description= $(git log -1 --pretty=%B)
+git commit -am 'Created Tag '"$tag_version"' with '$commit_description''
 git tag $tag_version -m "$commit_description"
 git push origin release-$tag_version
 git push origin $tag_version --force
