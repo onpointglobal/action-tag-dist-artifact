0f68c6f975a770dc6a2c8635992df4c4619516a6
Renzo Mayer <rmayer@onpointglobal.com>
2021-04-05 14:32:07 -0300

    create and add tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,6 +47,6 @@ git rm -r --cached .
 git add .
 MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 git commit -am 'Created Tag '"$tag_version"' with '"$MESSAGE"''
-git tag "$tag_version" -m "$MESSAGE"
-git push "$tag_version"
-git push origin release-$tag_version --force
+git tag v"$tag_version" -m "$MESSAGE"
+# git push "$tag_version"
+git push origin release-$tag_version --follow-tags
